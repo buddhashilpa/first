0dbec50db2fc8e5ffdd317f618b6cdf2a913b8fd
buddhashilpa <shilpabuddha4@gmail.com>
2016-02-18 10:09:56 +0530

    Initial Commit

---
+++ submit.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<title>
+registration</title>
+<body>
+<?php
+Name:
+<input type="text" name="text1" >
+Father's Name:
+<input type="text" name="text1" >
+Phone No:
+<input type="text" name="text1" >
+City
+<input type="text" name="text1" >
+State
+<input type="text" name="text1" >
+Nationality:
+<input type="text" name="text1" >
+Email Id:
+<input type="text" name="text1" >
+<button>Submit</button>
+?>
+</body>
+</html>
+
